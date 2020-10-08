bdc50ac8979f12f16ee20baa81262ebae699061b
Axonny <sasha.2002056@gmail.com>
2020-10-08 14:01:18 +0500

    update pinout and replace loops

--- lab1/lab1.ino
+++ lab1/lab1.ino
@@ -1,23 +1,11 @@
-#define LED_128 0
-#define LED_64 0
-#define LED_32 0
-#define LED_16 0
-#define LED_8 0
-#define LED_4 0
-#define LED_2 0
-#define LED_1 0
-
+int LED[8] = {0, 0, 0, 0 ,0, 0, 0, 0}; // 1 2 4 8 16 32 64 128
 
 void setup()
 {
-  pinMode(LED_128, OUTPUT);
-  pinMode(LED_64, OUTPUT);
-  pinMode(LED_32, OUTPUT);
-  pinMode(LED_16, OUTPUT);
-  pinMode(LED_8, OUTPUT);
-  pinMode(LED_4, OUTPUT);
-  pinMode(LED_2, OUTPUT);
-  pinMode(LED_1, OUTPUT);
+  for(int i = 0; i < 8; i++)
+  {
+    pinMode(LED[i], OUTPUT);
+  }
 }
 
 void loop() 
@@ -39,12 +27,8 @@ bool is_bit_set(int number, int bit)
 
 void num_to_led(int num)
 {
-  digitalWrite(LED_128, is_bit_set(num, 7));
-  digitalWrite(LED_64, is_bit_set(num, 6));
-  digitalWrite(LED_32, is_bit_set(num, 5));
-  digitalWrite(LED_16, is_bit_set(num, 4));
-  digitalWrite(LED_8, is_bit_set(num, 3));
-  digitalWrite(LED_4, is_bit_set(num, 2));
-  digitalWrite(LED_2, is_bit_set(num, 1));
-  digitalWrite(LED_1, is_bit_set(num, 0));
+  for(int i = 0; i < 8; i++)
+  {
+    digitalWrite(LED[i], is_bit_set(num, i));
+  }
 }
