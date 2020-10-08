989b77a621b5d1001c1b08efd35d6aae1fc75edd
Axonny <sasha.2002056@gmail.com>
2020-10-08 18:50:38 +0500

    fix bugs

--- lab1/lab1.ino
+++ lab1/lab1.ino
@@ -2,6 +2,7 @@ int LED[8] = {6, 7, 8, 9 ,10, 11, 12, 13}; // 1 2 4 8 16 32 64 128
 
 void setup()
 {
+  Serial.begin(115200);
   for(int i = 0; i < 8; i++)
   {
     pinMode(LED[i], OUTPUT);
@@ -11,13 +12,15 @@ void setup()
 void loop() 
 {
   int number = readInput();
+  Serial.println(number);
   num_to_led(number);
 }
 
 int readInput()
 {
-    while (!Serial.available());
-    return Serial.parseInt();
+    while(!Serial.available());
+    String input = Serial.readStringUntil('\n');
+    return input.toInt();
 }
 
 bool is_bit_set(int number, int bit)
@@ -29,6 +32,6 @@ void num_to_led(int num)
 {
   for(int i = 0; i < 8; i++)
   {
-    digitalWrite(LED[i], is_bit_set(num, i));
+    digitalWrite(LED[i], 1 - is_bit_set(num, i));
   }
 }
