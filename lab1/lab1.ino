2e136c531f24de2904819ff29724ae921c456fbe
Your Name <v.starostina@goostaff.ru>
2020-10-09 18:21:02 +0500

    task 10

--- lab1/lab1.ino
+++ lab1/lab1.ino
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <Arduino.h>
 #include <MD_TCS230.h>
 
@@ -10,6 +9,9 @@
 #define R_OUT 6
 #define G_OUT 7
 #define B_OUT 8
+#define PIN_BUTTON_OFF 5
+
+Button buttonOff(PIN_BUTTON_OFF);
 
 MD_TCS230 colorSensor(S2_OUT, S3_OUT, S0_OUT, S1_OUT);
 
@@ -64,33 +66,5 @@ void set_rgb_led(colorData rgb)
     analogWrite(R_OUT, 255 - rgb.value[TCS230_RGB_R]);
     analogWrite(G_OUT, 255 - rgb.value[TCS230_RGB_G]);
     analogWrite(B_OUT, 255 - rgb.value[TCS230_RGB_B]);
-=======
-#include "pitches.h"
-#include "button.h"
-#include "buzzer.h"
-
-#define PIN_BUZZER 6
-#define PIN_BUTTON_OFF 5
-
-Button buttonOff(PIN_BUTTON_OFF);
-Buzzer buzzer(PIN_BUZZER);
-
-
-int notes[] = {NOTE_G3, NOTE_SILENCE, NOTE_G3, NOTE_SILENCE, NOTE_G3, NOTE_SILENCE, NOTE_DS3, NOTE_SILENCE};
-double durations[] = {8, 8, 1, 8, 1, 8, 1, 24};
-int melodyLength = 8;
-
-void setup() {
-    buzzer.setMelody(notes, durations, melodyLength);
-    buzzer.turnSoundOn();
-}
-
-void loop() {
-  
-    buzzer.playSound();
-    if (buttonOff.wasPressed())
-    {
-        buzzer.turnSoundOff();
-    }
->>>>>>> buzzer-and-buttons
+    //    if (buttonOff.wasPressed())
 }
