4017c77bb0e5af2318f6b4d6457ff37ac5d4ec17
An <anna.makarova@saritasa.com>
2020-10-10 14:20:28 +0500

    del methods

--- lab1/lab1.ino
+++ lab1/lab1.ino
@@ -23,38 +23,23 @@ void setup()
 
 }
 
+
 void loop()
 {
-  colorData rgb;
-  for (int i = 0; i < 6; i++)
-  {
-    set_red(rgb);
-    delay(400);
-    set_white(rgb);
-    delay(100)
+  if (buttonOn.wasPressed()) {
+    for (int i = 0; i < 6; i++) {
+      set_rgb_led(255, 0, 0);
+      delay(4000);
+      set_rgb_led(255, 255, 255);
+      delay(1000);
+    }
+    set_rgb_led(0, 0, 255);
   }
-  set_blue(rgb);
+}
 
-void set_rgb_led(colorData rgb)
+void set_rgb_led(int r, int g, int b)
 {
-  analogWrite(R_OUT, 255 - rgb.value[TCS230_RGB_R]);
-  analogWrite(G_OUT, 255 - rgb.value[TCS230_RGB_G]);
-  analogWrite(B_OUT, 255 - rgb.value[TCS230_RGB_B]);
-}
-
-void set_red_color(colorData rgb) {
-  rgb.value[TCS230_RGB_R] = 255;
-  set_rgb_led(rgb);
-}
-
-void set_blue_color(colorData rgb) {
-  rgb.value[TCS230_RGB_B] = 255;
-  set_rgb_led(rgb);
-}
-
-void set_white_color(colorData rgb) {
-  rgb.value[TCS230_RGB_R] = 255;
-  rgb.value[TCS230_RGB_G] = 255;
-  rgb.value[TCS230_RGB_B] = 255;
-  set_rgb_led(rgb);
+  analogWrite(R_OUT, 255 - r);
+  analogWrite(G_OUT, 255 - g);
+  analogWrite(B_OUT, 255 - b);
 }
