0bdb1ba7a65a999aa49f97945c29fbc3b7d01836
cutletmedie <sjkey@mail.ru>
2020-10-10 15:34:37 +0500

    Update lab1.ino

--- lab1/lab1.ino
+++ lab1/lab1.ino
@@ -1,12 +1,30 @@
+#define ledsCount 6
 
-#define R_OUT 6
-
-void setup()
-{
-
+void ledPin(int firstLed, int secondLed){
+  for (int fadeValue = 0 ; fadeValue <= 255; fadeValue += 5) {
+    analogWrite(firstLed, fadeValue);
+    analogWrite(secondLed, fadeValue);
+    delay(30);
+  }
+  for (int fadeValue = 255 ; fadeValue >= 0; fadeValue -= 5) {
+    analogWrite(firstLed, fadeValue);
+    analogWrite(secondLed, fadeValue);
+    delay(30);
+  }
 }
 
-void loop() 
-{
+int leds[ledsCount] = {2, 3, 4, 5, 6, 7};
+void setup() {
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+}
 
+void loop() {
+  for (int i = 0; i < 3; i++){
+    ledPin(leds[i],leds[i+3]);
+  }
 }
