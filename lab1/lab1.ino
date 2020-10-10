6db054bd21dff89d83ee162b488b7e0b690c67b0
An <anna.makarova@saritasa.com>
2020-10-10 12:29:29 +0500

    set colors

--- lab1/lab1.ino
+++ lab1/lab1.ino
@@ -9,58 +9,52 @@
 #define R_OUT 6
 #define G_OUT 7
 #define B_OUT 8
+#define PIN_BUTTON_ON 9
+
+Button buttonOn(PIN_BUTTON_ON);
 
 MD_TCS230 colorSensor(S2_OUT, S3_OUT, S0_OUT, S1_OUT);
 
 void setup()
 {
-    Serial.begin(115200);
-    Serial.println("Started!");
+  pinMode(R_OUT, OUTPUT);
+  pinMode(G_OUT, OUTPUT);
+  pinMode(B_OUT, OUTPUT);
 
-    sensorData whiteCalibration;
-    whiteCalibration.value[TCS230_RGB_R] = 0;
-    whiteCalibration.value[TCS230_RGB_G] = 0;
-    whiteCalibration.value[TCS230_RGB_B] = 0;
-
-    sensorData blackCalibration;
-    blackCalibration.value[TCS230_RGB_R] = 0;
-    blackCalibration.value[TCS230_RGB_G] = 0;
-    blackCalibration.value[TCS230_RGB_B] = 0;
-
-    colorSensor.begin();
-    colorSensor.setDarkCal(&blackCalibration);
-    colorSensor.setWhiteCal(&whiteCalibration);
-
-    pinMode(R_OUT, OUTPUT);
-    pinMode(G_OUT, OUTPUT);
-    pinMode(B_OUT, OUTPUT);
 }
 
-void loop() 
+void loop()
 {
-    colorData rgb;
-    colorSensor.read();
-
-    while (!colorSensor.available());
-
-    colorSensor.getRGB(&rgb);
-    print_rgb(rgb);
-    set_rgb_led(rgb);
-}
-
-void print_rgb(colorData rgb)
-{
-  Serial.print(rgb.value[TCS230_RGB_R]);
-  Serial.print(" ");
-  Serial.print(rgb.value[TCS230_RGB_G]);
-  Serial.print(" ");
-  Serial.print(rgb.value[TCS230_RGB_B]);
-  Serial.println();
-}
+  colorData rgb;
+  for (int i = 0; i < 6; i++)
+  {
+    set_red(rgb);
+    delay(400);
+    set_white(rgb);
+    delay(100)
+  }
+  set_blue(rgb);
 
 void set_rgb_led(colorData rgb)
 {
-    analogWrite(R_OUT, 255 - rgb.value[TCS230_RGB_R]);
-    analogWrite(G_OUT, 255 - rgb.value[TCS230_RGB_G]);
-    analogWrite(B_OUT, 255 - rgb.value[TCS230_RGB_B]);
+  analogWrite(R_OUT, 255 - rgb.value[TCS230_RGB_R]);
+  analogWrite(G_OUT, 255 - rgb.value[TCS230_RGB_G]);
+  analogWrite(B_OUT, 255 - rgb.value[TCS230_RGB_B]);
+}
+
+void set_red_color(colorData rgb) {
+  rgb.value[TCS230_RGB_R] = 255;
+  set_rgb_led(rgb);
+}
+
+void set_blue_color(colorData rgb) {
+  rgb.value[TCS230_RGB_B] = 255;
+  set_rgb_led(rgb);
+}
+
+void set_white_color(colorData rgb) {
+  rgb.value[TCS230_RGB_R] = 255;
+  rgb.value[TCS230_RGB_G] = 255;
+  rgb.value[TCS230_RGB_B] = 255;
+  set_rgb_led(rgb);
 }
