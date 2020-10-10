8d575abae37acebc3488ab5fb669b1bfab0eba13
Your Name <v.starostina@goostaff.ru>
2020-10-10 14:08:39 +0500

    fix 2

--- lab1/lab1.ino
+++ lab1/lab1.ino
@@ -25,52 +25,20 @@ void setup()
 
 void loop()
 {
-  colorData rgb;
   if (buttonOn.wasPressed()) {
     for (int i = 0; i < 5; i++) {
-      set_red_color(rgb);
+      set_rgb_led(255, 0, 0);
       delay(500);
-      set_white_color(rgb);
+      set_rgb_led(255, 255, 255);
       delay(100);
     }
-    set_blue_color(rgb);
+    set_rgb_led(0, 0, 255);
   }
 }
 
-//void print_rgb(colorData rgb)
-//{
-//  Serial.print(rgb.value[TCS230_RGB_R]);
-//  Serial.print(" ");
-//  Serial.print(rgb.value[TCS230_RGB_G]);
-//  Serial.print(" ");
-//  Serial.print(rgb.value[TCS230_RGB_B]);
-//  Serial.println();
-//}
-
-void set_rgb_led(colorData rgb)
+void set_rgb_led(int r, int g, int b)
 {
-  analogWrite(R_OUT, 255 - rgb.value[TCS230_RGB_R]);
-  analogWrite(G_OUT, 255 - rgb.value[TCS230_RGB_G]);
-  analogWrite(B_OUT, 255 - rgb.value[TCS230_RGB_B]);
-}
-
-void set_rgb_color(colorData rgb, int r, int g, int b) {
-  rgb.value[TCS230_RGB_R] = r;
-  rgb.value[TCS230_RGB_G] = g;
-  rgb.value[TCS230_RGB_B] = b;
-}
-
-void set_red_color(colorData rgb) {
-  set_rgb_color(rgb, 255, 0, 0);
-  set_rgb_led(rgb);
-}
-
-void set_white_color(colorData rgb) {
-  set_rgb_color(rgb, 255, 255, 255);
-  set_rgb_led(rgb);
-}
-
-void set_blue_color(colorData rgb) {
-  set_rgb_color(rgb, 0, 0, 255);
-  set_rgb_led(rgb);
+  analogWrite(R_OUT, 255 - r);
+  analogWrite(G_OUT, 255 - g);
+  analogWrite(B_OUT, 255 - b);
 }
