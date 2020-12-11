40e10493186c1c60421e4208a647ade663d413a1
shemloft <shem.dar99@gmail.com>
2020-12-11 23:59:43 +0500

    Решение задачки

--- lab1/lab1.ino
+++ lab1/lab1.ino
@@ -2,18 +2,22 @@
 #include "button.h"
 #include "buzzer.h"
 
+#define PIN_BUTTON 5
 #define PIN_BUZZER 6
-#define PIN_BUTTON_OFF 5
-#define R_OUT 6
-#define G_OUT 7
-#define B_OUT 8
+#define R_OUT 7
+#define G_OUT 8
+#define B_OUT 9
 
-Button buttonOff(PIN_BUTTON_OFF);
+Button button(PIN_BUTTON);
 Buzzer buzzer(PIN_BUZZER);
 
-int notes[] = {NOTE_G3, NOTE_SILENCE, NOTE_G3, NOTE_SILENCE, NOTE_G3, NOTE_SILENCE, NOTE_DS3, NOTE_SILENCE};
-double durations[] = {8, 8, 1, 8, 1, 8, 1, 24};
-int melodyLength = 8;
+int notes[] = {NOTE_G3};
+double durations[] = {1};
+int melodyLength = 1;
+
+int r = 255;
+int g = 0;
+int b = 0;
 
 void setup()
 {
@@ -24,18 +28,27 @@ void setup()
     buzzer.turnSoundOn();
 }
 
-void loop() 
+void loop()
 {
-  buzzer.playSound();
-    if (buttonOff.wasPressed())
+    if (button.wasPressed())
     {
-        buzzer.turnSoundOff();
+        make_shot();
     }
 }
 
-void set_rgb_led(colorData rgb)
+void make_shot()
 {
-    analogWrite(R_OUT, 255 - rgb.value[TCS230_RGB_R]);
-    analogWrite(G_OUT, 255 - rgb.value[TCS230_RGB_G]);
-    analogWrite(B_OUT, 255 - rgb.value[TCS230_RGB_B]);
+    buzzer.turnSoundOn();
+    buzzer.playSound();
+    set_rgb_led(r, g, b);
+    delay(100);
+    buzzer.turnSoundOff();
+    set_rgb_led(0, 0, 0);
+}
+
+void set_rgb_led(int r, int g, int b)
+{
+    analogWrite(R_OUT, 255 - r);
+    analogWrite(G_OUT, 255 - g);
+    analogWrite(B_OUT, 255 - b);
 }
