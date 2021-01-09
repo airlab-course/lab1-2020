2c5a68714d28ac83d9a53e5af5fd833fe01b77c1
vaspahomov <vas2142553@gmail.com>
2021-01-09 16:19:46 +0500

    lab1

--- lab1/lab1.ino
+++ lab1/lab1.ino
@@ -1,87 +1,73 @@
 #include <Arduino.h>
 #include <MD_TCS230.h>
-#include "pitches.h"                                                                                                    
 #include "button.h"                                                                                                     
-#include "buzzer.h" 
 
-#define  S0_OUT  2
-#define  S1_OUT  3
-#define  S2_OUT  4
-#define  S3_OUT  5
+#define S0_OUT  2
+#define S1_OUT  3
+#define S2_OUT  4
+#define S3_OUT  5
 
 #define R_OUT 6
 #define G_OUT 7
 #define B_OUT 8
 
-#define PIN_BUZZER 6                                                                                                    
-#define PIN_BUTTON_OFF 5                                                                                                
+#define PIN_BUTTON_ON 9
 
-Button buttonOff(PIN_BUTTON_OFF);                                                                                       
-Buzzer buzzer(PIN_BUZZER);                                                                                              
-                                                                                                                         
-                                                                                                                         
-int notes[] = {NOTE_G3, NOTE_SILENCE, NOTE_G3, NOTE_SILENCE, NOTE_G3, NOTE_SILENCE, NOTE_DS3, NOTE_SILENCE};            
-double durations[] = {8, 8, 1, 8, 1, 8, 1, 24};                                                                         
-int melodyLength = 8;                                 
-
+Button buttonOn(PIN_BUTTON_ON);
 MD_TCS230 colorSensor(S2_OUT, S3_OUT, S0_OUT, S1_OUT);
 
 void setup()
 {
-    Serial.begin(115200);
-    Serial.println("Started!");
+	Serial.begin(115200);
+	Serial.println("Started!");
 
-    sensorData whiteCalibration;
-    whiteCalibration.value[TCS230_RGB_R] = 0;
-    whiteCalibration.value[TCS230_RGB_G] = 0;
-    whiteCalibration.value[TCS230_RGB_B] = 0;
+	sensorData whiteCalibration;
+	whiteCalibration.value[TCS230_RGB_R] = 0;
+	whiteCalibration.value[TCS230_RGB_G] = 0;
+	whiteCalibration.value[TCS230_RGB_B] = 0;
 
-    sensorData blackCalibration;
-    blackCalibration.value[TCS230_RGB_R] = 0;
-    blackCalibration.value[TCS230_RGB_G] = 0;
-    blackCalibration.value[TCS230_RGB_B] = 0;
+	sensorData blackCalibration;
+	blackCalibration.value[TCS230_RGB_R] = 0;
+	blackCalibration.value[TCS230_RGB_G] = 0;
+	blackCalibration.value[TCS230_RGB_B] = 0;
 
-    colorSensor.begin();
-    colorSensor.setDarkCal(&blackCalibration);
-    colorSensor.setWhiteCal(&whiteCalibration);
+	colorSensor.begin();
+	colorSensor.setDarkCal(&blackCalibration);
+	colorSensor.setWhiteCal(&whiteCalibration);
 
-    pinMode(R_OUT, OUTPUT);
-    pinMode(G_OUT, OUTPUT);
-    pinMode(B_OUT, OUTPUT);
+	pinMode(R_OUT, OUTPUT);
+	pinMode(G_OUT, OUTPUT);
+	pinMode(B_OUT, OUTPUT);
 }
 
 void loop() 
 {
-    colorData rgb;
-    colorSensor.read();
-
-    while (!colorSensor.available());
-
-    colorSensor.getRGB(&rgb);
-    print_rgb(rgb);
-    set_rgb_led(rgb);
-    buzzer.playSound();                                                           
-    if (buttonOff.wasPressed())                                                                                         
-    {                                                                                                                   
-        buzzer.turnSoundOff();                                                                                          
-    }
-    buzzer.setMelody(notes, durations, melodyLength);
-    buzzer.turnSoundOn();
+	colorData rgb;
+	if (buttonOn.wasPressed()) {
+		colorSensor.read();
+		while (!colorSensor.available());
+		colorSensor.getRGB(&rgb);
+		print_rgb(rgb);
+		set_rgb_led(rgb);
+	} else {
+		print_rgb(rgb);                                                                                         
+		set_rgb_led(rgb);
+	}
 }
 
 void print_rgb(colorData rgb)
 {
-  Serial.print(rgb.value[TCS230_RGB_R]);
-  Serial.print(" ");
-  Serial.print(rgb.value[TCS230_RGB_G]);
-  Serial.print(" ");
-  Serial.print(rgb.value[TCS230_RGB_B]);
-  Serial.println();
+	Serial.print(rgb.value[TCS230_RGB_R]);
+	Serial.print(" ");
+	Serial.print(rgb.value[TCS230_RGB_G]);
+	Serial.print(" ");
+	Serial.print(rgb.value[TCS230_RGB_B]);
+	Serial.println();
 }
 
 void set_rgb_led(colorData rgb)
 {
-    analogWrite(R_OUT, 255 - rgb.value[TCS230_RGB_R]);
-    analogWrite(G_OUT, 255 - rgb.value[TCS230_RGB_G]);
-    analogWrite(B_OUT, 255 - rgb.value[TCS230_RGB_B]);
+	analogWrite(R_OUT, 255 - rgb.value[TCS230_RGB_R]);
+	analogWrite(G_OUT, 255 - rgb.value[TCS230_RGB_G]);
+	analogWrite(B_OUT, 255 - rgb.value[TCS230_RGB_B]);
 }
