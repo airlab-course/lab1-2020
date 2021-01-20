6c13ada84e0e9eb02cfae5fafae93658b52e281c
gladozik <gladozik1@gmail.com>
2021-01-20 21:33:45 +0500

    Fixing game restart if nobody touched the button

--- lab1/lab1.ino
+++ lab1/lab1.ino
@@ -1,39 +1,40 @@
-#define PIN_LED 12  // пин с пищалкой
-#define PLAYER_COUNT 2   // количество игроков-ковбоев
-// вместо перечисления всех пинов по-одному, мы объявляем пару
-// списков: один с номерами пинов с кнопками, другой — со
-// светодиодами. Списки также называют массивами (англ. array)
-int buttonPins[PLAYER_COUNT] = {3, 13};
-int ledPins[PLAYER_COUNT] = {9, 11};
+#define PIN_LED 12  
+#define BUZZER_PIN 10 
+#include "buzzer.h"
+
+int buttonPin[2] = {3, 13};
+int ledPin[2] = {9, 11};
 int randomNumber;
 void setup()
 {
   pinMode(PIN_LED, OUTPUT);
-  for (int player = 0; player < PLAYER_COUNT; ++player) {
-    // при помощи квадратных скобок получают значение в массиве
-    // под указанным в них номером. Нумерация начинается с нуля
-    pinMode(ledPins[player], OUTPUT);
-    pinMode(buttonPins[player], INPUT_PULLUP);
+  pinMode(BUZZER_PIN, OUTPUT);
+  
+  for (int player = 0; player < 2; ++player) {
+    pinMode(ledPin[player], OUTPUT);
+    pinMode(buttonPin[player], INPUT_PULLUP);
   }
+
 }
  
 void loop()
 {
-  // даём сигнал «пли!», выждав случайное время от 2 до 7 сек
   randomNumber = random(2000,6000);
   delay(randomNumber);
   digitalWrite(PIN_LED, HIGH);
- 
-  for (int player = 0; ; player = (player+1) % PLAYER_COUNT) {
-    // если игрок номер «player» нажал кнопку...
-    if (!digitalRead(buttonPins[player])) {
-      // ...включаем его светодиод и сигнал победы на 1 сек
-      digitalWrite(ledPins[player], HIGH);
+  for (int player = 0; ; player = (player+1) % 2) {
+    if (!digitalRead(buttonPin[player])) {
+      digitalWrite(ledPin[player], HIGH);
+      tone(BUZZER_PIN, 4000, 1000);
       digitalWrite(PIN_LED, LOW);
-
       delay(1000);
-      digitalWrite(ledPins[player], LOW);
-      break; // Есть победитель! Выходим (англ. break) из цикла
+      digitalWrite(ledPin[player], LOW);
+    buzzer.playSound();
+    if (buttonOff.wasPressed())
+    {
+        buzzer.turnSoundOff(); 
+    }
+      break; 
     }
   }
 }
